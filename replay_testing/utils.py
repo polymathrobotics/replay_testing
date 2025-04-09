f39037f5244084a369eee1117db880bcab3ea2a3
Troy Gibb <troy.gibb@gmail.com>
2025-04-09 21:36:51 +0000

    Update licensing

--- replay_testing/utils.py
+++ replay_testing/utils.py
@@ -1,10 +1,16 @@
 # Copyright (c) 2025-present Polymath Robotics, Inc.
 #
-# Permission is hereby granted to use, copy, modify, and distribute this software
-# in source or binary form, provided that the above copyright notice and this
-# permission notice appear in all copies or substantial portions of the software.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This software is provided "as is", without warranty of any kind.
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 
 
